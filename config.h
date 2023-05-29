21bdc0fefd71d03b46227e5bea977010716bfe8d
“Glenn <glenn.hoeppner@yakhair.com>
2023-05-29 18:14:54 -0400

    Remove STDC_HEADERS check
    This test is no longer necessary, it always exists.

--- config.h
+++ config.h
@@ -120,11 +120,6 @@
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
 
-/* Define to 1 if all of the C90 standard headers exist (not just the ones
-   required in a freestanding environment). This macro is provided for
-   backward compatibility; new code need not use it. */
-#define STDC_HEADERS 1
-
 /* Define to 1 if you can safely include both <sys/time.h> and <time.h>. This
    macro is obsolete. */
 #define TIME_WITH_SYS_TIME 1
--- defs.h
+++ defs.h
@@ -11,9 +11,7 @@
 #define INTVERSION	239
 #include "config.h"
 
-#ifdef STDC_HEADERS
 #include <stdlib.h>
-#endif
 #include <sys/types.h>
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
