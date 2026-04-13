24148edba6d5777976af4fbbb8b9565a532e46bd
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2026-04-12 22:26:51 -0400

    Move error handling code into its own file
    fatal error, perror, shutdown, tempfile error

--- utility.c
+++ utility.c
@@ -120,54 +120,6 @@ void replyMessage( void )
    sendingXState = SX_NOT;
 }
 
-noreturn void fatalPerror( const char *error, const char *heading )
-{
-   int savedErrno = errno;
-
-   fflush( stdout );
-   errno = savedErrno;
-   sPerror( error, heading );
-   myExit();
-}
-
-noreturn void fatalExit( const char *message, const char *heading )
-{
-   fflush( stdout );
-   sError( message, heading );
-   myExit();
-}
-
-noreturn void myExit( void )
-{
-   fflush( stdout );
-   if ( childPid )
-   {
-      /* Wait for child to terminate */
-      sigOff();
-      childPid = ( -childPid );
-      while ( childPid )
-      {
-         sigpause( 0 );
-      }
-   }
-   resetTerm();
-#ifdef HAVE_OPENSSL
-   if ( isSsl )
-   {
-      killSsl();
-   }
-#endif
-   if ( flagsConfiguration.isLastSave )
-   {
-      if ( !( tempFile = freopen( aryTempFileName, "w+", tempFile ) ) )
-      {
-         sPerror( "myExit: reopen temp file before exit", "Shutdown warning" );
-      }
-   }
-   deinitialize();
-   exit( 0 );
-}
-
 void looper( void )
 {
    unsigned int invalid = 0;
@@ -344,16 +296,6 @@ int yesNoDefault( int defaultAnswer )
       fatalExit( aryBuffer, "Internal error" );
    }
    return 0;
-}
-
-void tempFileError( void )
-{
-   if ( errno == EINTR )
-   {
-      return;
-   }
-   fprintf( stderr, "\r\n" );
-   sPerror( "writing tempfile", "Local error" );
 }
 
 int more( int *line, int percentComplete )
